2188c186aa1a5061aa4a7f28590ad390d3b3fdd9
“usa0415” <usatarou88@gmail.com>
2021-04-25 21:57:54 +0900

    deploy9

--- new_learn_mgr/settings.py
+++ new_learn_mgr/settings.py
@@ -82,16 +82,7 @@ WSGI_APPLICATION = 'new_learn_mgr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES =  {'default': {}}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
