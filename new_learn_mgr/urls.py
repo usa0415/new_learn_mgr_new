5c0eb5ff4488e4225a965a4e2b85400da68c8952
“usa0415” <usatarou88@gmail.com>
2021-04-26 21:56:10 +0900

    deploy14

--- new_learn_mgr/urls.py
+++ new_learn_mgr/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('ioput.urls')),
     path('admin/', admin.site.urls),
-    path('ioput/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     #path('', include('accounts.urls')),
 ]
 
